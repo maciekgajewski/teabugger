ff8bb3a94327b08fb430790b79bc5cc0c7790383
Maciej Gajewski <maciej.gajewski0@gmail.com>
2016-08-17 15:37:31 +0200

    work started o parsing GDBs output

--- teabugger-python/teabugger/gdb.py
+++ teabugger-python/teabugger/gdb.py
@@ -10,14 +10,23 @@ class Gdb:
 		print("Created GDB session")
 
 		args=['gdb', '--interpreter=mi', binary]
-		self.process = Popen(args, stdin=PIPE, stdout=PIPE)
+		self.process = Popen(args, stdin=PIPE, stdout=PIPE, universal_newlines=True)
 
 		self.read()
 
 	def read(self):
 		""" Read all input from the debugger """
 		for line in self.process.stdout:
-			print("read line: %s" % line)
+			stripped = line.strip() # TODO must a clever, pythonish way to do it in the for statemen
+			
+			if stripped == '(gdb)':
+				return
+			self.processLine(stripped)
 
+	def processLine(self, line):
+		type = line[0]
+		text = line[1:]
+
+		print('type=%s, text=%s' % (type, text))
 
 
