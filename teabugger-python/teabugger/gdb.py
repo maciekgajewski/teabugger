321b5d37ec1e3f8d622e39c09eda092d752e4791
Maciej Gajewski <maciej.gajewski0@gmail.com>
2016-08-26 11:16:24 +0000

    parsing output

--- teabugger-python/teabugger/gdb.py
+++ teabugger-python/teabugger/gdb.py
@@ -1,5 +1,73 @@
 from subprocess import Popen, PIPE
 
+def parseAsyncOutput(text):
+	record = {}
+	commaPos = text.find(',')
+	if commaPos == -1:
+		record['class'] = text
+	else:
+		record['class'] = text[:commaPos]
+		result, _ = parseResult(text[commaPos+1:])
+		record['result'] = result
+	
+	return record
+
+def parseResult(text):
+	eqPos = text.find('=')
+	key = text[:eqPos]
+	value, l = parseValue(text[eqPos+1:])
+	return { key : value }, eqPos+l+1
+
+def parseValue(text):
+	if text[0] == '"':
+		return parseString(text)
+	elif text[0] == "[":
+		return parseList(text)
+	elif text[0] == "{":
+		return parseTuple(text)
+	else:
+		raise ValueError("unable to parse value: $text")
+
+def parseString(text):
+	if text[0] != '"':
+		raise ValueError("Error parsing string")
+	
+	lastQuote = text.find('"', 1)
+	return text[1:lastQuote], lastQuote+1
+	
+def parseList(text):
+	assert text[0] == '['
+	pos = 1
+	result = []
+	while True:
+		r, l = parseValue(text[pos:])
+		result.append(r)
+		pos = pos + l
+		if text[pos] == ',':
+			pos = pos+1
+			continue
+		elif text[pos] == ']':
+			return result, pos+1
+		raise ValueError("Error parsing list %s" % text[pos:])
+		
+	
+def parseTuple(text):
+	if text[0] != '{':
+		raise ValueError("Error parsing tuple")
+	result = {}
+	pos = 1
+	while True:
+		r, l = parseResult(text[pos:])
+		result.update(r)
+		pos = pos + l
+		if text[pos] == ',':
+			pos = pos+1
+			continue
+		elif text[pos] == '}':
+			return result, pos+1
+		raise ValueError("Error parsing tuple %s" % text[pos:])
+		
+
 
 class Gdb:
 	"""GDB Session"""
@@ -15,18 +83,45 @@ class Gdb:
 		self.read()
 
 	def read(self):
-		""" Read all input from the debugger """
+		""" Read all input from the debugger, untile the "(gdb)" marker """
+		result = []
 		for line in self.process.stdout:
 			stripped = line.strip() # TODO must a clever, pythonish way to do it in the for statemen
 			
 			if stripped == '(gdb)':
 				return
-			self.processLine(stripped)
+			r = self.processLine(stripped)
+			result.append(r)
+		return result
 
+	def send(self, command):
+		""" Sends command to GDB, returns result """
+		self.process.stdin.write(command+"\n")
+		self.process.stdin.flush()
+		return self.read()
+		
+	def command(self, command):
+		""" Sends command, retruns (result-record, oob-records) """
+		result = None
+		oobs = []
+		
+		records = self.send(command)
+		for r in records:
+			if r['type'] == '^':
+				result = r
+			else:
+				oobs.append(r)
+		
+		return result, oobs
+		
 	def processLine(self, line):
 		type = line[0]
 		text = line[1:]
 
-		print('type=%s, text=%s' % (type, text))
-
+		if type != '~':
+			#print('line %s' % line)
+			record = parseAsyncOutput(text)
+			record.update({'type':type})
+			print('record: %s' % record)
+			return record
 
--- teabugger-python/teabugger/runner.py
+++ teabugger-python/teabugger/runner.py
@@ -4,5 +4,6 @@ def runBinary(binary, output):
 	print("Hello, running %s" % binary, file=output)
 
 	gdb = _gdb.Gdb(binary)
-
+	r,o = gdb.command('-break-insert main')
+	print('result: %s' % r)
 	
